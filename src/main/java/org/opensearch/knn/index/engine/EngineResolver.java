21130a5e35ae4d7a4d2df5174734223267a5c780
Kunal Kotwani <kkotwani@amazon.com>
2025-02-06 01:28:04 +0000

    Fix log flooding for engine deprecation notice
    Signed-off-by: Kunal Kotwani <kkotwani@amazon.com>

--- src/main/java/org/opensearch/knn/index/engine/EngineResolver.java
+++ src/main/java/org/opensearch/knn/index/engine/EngineResolver.java
@@ -5,14 +5,19 @@
 
 package org.opensearch.knn.index.engine;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.opensearch.knn.index.mapper.CompressionLevel;
 import org.opensearch.knn.index.mapper.Mode;
+
+import static org.opensearch.knn.index.engine.KNNEngine.DEPRECATED_ENGINES;
 
 /**
  * Figures out what {@link KNNEngine} to use based on configuration details
  */
 public final class EngineResolver {
 
+    private static Logger logger = LogManager.getLogger(EngineResolver.class);
     public static final EngineResolver INSTANCE = new EngineResolver();
 
     private EngineResolver() {}
@@ -30,33 +35,39 @@ public final class EngineResolver {
         KNNMethodContext knnMethodContext,
         boolean requiresTraining
     ) {
+        KNNEngine knnEngine = KNNEngine.FAISS;
+
         // User configuration gets precedence
         if (knnMethodContext != null && knnMethodContext.isEngineConfigured()) {
-            return knnMethodContext.getKnnEngine();
+            knnEngine = knnMethodContext.getKnnEngine();
         }
 
         // Faiss is the only engine that supports training, so we default to faiss here for now
         if (requiresTraining) {
-            return KNNEngine.FAISS;
+            knnEngine = KNNEngine.FAISS;
         }
 
         Mode mode = knnMethodConfigContext.getMode();
         CompressionLevel compressionLevel = knnMethodConfigContext.getCompressionLevel();
         // If both mode and compression are not specified, we can just default
         if (Mode.isConfigured(mode) == false && CompressionLevel.isConfigured(compressionLevel) == false) {
-            return KNNEngine.DEFAULT;
+            knnEngine = KNNEngine.DEFAULT;
         }
 
         // For 1x, we need to default to faiss if mode is provided and use nmslib otherwise
         if (CompressionLevel.isConfigured(compressionLevel) == false || compressionLevel == CompressionLevel.x1) {
-            return mode == Mode.ON_DISK ? KNNEngine.FAISS : KNNEngine.NMSLIB;
+            knnEngine = mode == Mode.ON_DISK ? KNNEngine.FAISS : KNNEngine.NMSLIB;
         }
 
         // Lucene is only engine that supports 4x - so we have to default to it here.
         if (compressionLevel == CompressionLevel.x4) {
-            return KNNEngine.LUCENE;
+            knnEngine = KNNEngine.LUCENE;
         }
 
-        return KNNEngine.FAISS;
+        if (DEPRECATED_ENGINES.contains(knnEngine)) {
+            logger.warn("[Deprecation] {} engine is deprecated and will be removed in a future release.", knnEngine);
+        }
+
+        return knnEngine;
     }
 }
